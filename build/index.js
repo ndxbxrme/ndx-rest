d8b747c1779a89c0877266631b8fad3761d34dc7
ndxbxrme <lewis_the_cat@hotmail.com>
2017-03-06 01:09:24 +0000

    upgrades

--- build/index.js
+++ build/index.js
@@ -4,6 +4,20 @@
     ndx.rest = {};
     return setImmediate(function() {
       var auth, deleteFn, i, len, ref, results, selectFn, table, tableName, type, upsertFn;
+      ndx.app.get('/rest/endpoints', function(req, res, next) {
+        var endpoint, endpoints, i, len;
+        endpoints = ndx.rest.tables || ndx.settings.REST_TABLES || ndx.settings.TABLES;
+        if (endpoints && endpoints.length && Object.prototype.toString.call(endpoints[0]) === '[object Array]') {
+          for (i = 0, len = endpoints.length; i < len; i++) {
+            endpoint = endpoints[i];
+            endpoint = endpoint[0];
+          }
+        }
+        return res.json({
+          autoId: ndx.settings.AUTO_ID,
+          endpoints: endpoints
+        });
+      });
       ref = ndx.rest.tables || ndx.settings.REST_TABLES || ndx.settings.TABLES;
       results = [];
       for (i = 0, len = ref.length; i < len; i++) {
@@ -17,54 +31,62 @@
           tableName = table[0];
           auth = table[1];
         }
-        selectFn = function(req, res, next) {
-          var items, where;
-          if (ndx.permissions && !ndx.permissions.check('select', req.user)) {
-            return next('Not permitted');
-          }
-          if (req.params && req.params.id) {
-            where = {};
-            where[ndx.settings.AUTO_ID] = req.params.id;
-            items = ndx.database.select(tableName, where);
-            if (items && items.length) {
-              return res.json(items[0]);
-            } else {
-              return res.end('Nothing found');
+        selectFn = function(tableName) {
+          return function(req, res, next) {
+            var items, where;
+            if (ndx.permissions && !ndx.permissions.check('select', req.user)) {
+              return next('Not permitted');
             }
-          } else {
-            return res.json({
-              total: ndx.database.count(tableName, req.body.where),
-              page: req.body.page || 1,
-              pageSize: req.body.pageSize || 0,
-              items: ndx.database.select(tableName, req.body.where, req.body.page, req.body.pageSize, req.body.sort, req.body.sortDir)
-            });
-          }
+            if (req.params && req.params.id) {
+              where = {};
+              where[ndx.settings.AUTO_ID] = req.params.id;
+              items = ndx.database.select(tableName, where);
+              if (items && items.length) {
+                return res.json(items[0]);
+              } else {
+                return res.end('Nothing found');
+              }
+            } else {
+              return res.json({
+                total: ndx.database.count(tableName, req.body.where),
+                page: req.body.page || 1,
+                pageSize: req.body.pageSize || 0,
+                items: ndx.database.select(tableName, req.body.where, req.body.page, req.body.pageSize, req.body.sort, req.body.sortDir)
+              });
+            }
+          };
         };
-        upsertFn = function(req, res, next) {
-          var op;
-          op = req.params.id ? 'update' : 'insert';
-          if (ndx.permissions && !ndx.permissions.check(op, req.user)) {
-            return next('Not permitted');
-          }
-          if (req.params.id) {
-            req.body[ndx.settings.AUTO_ID] = req.params.id;
-          }
-          ndx.database.upsert(tableName, req.body);
-          return res.end('OK');
+        upsertFn = function(tableName) {
+          return function(req, res, next) {
+            var op, where;
+            op = req.params.id ? 'update' : 'insert';
+            if (ndx.permissions && !ndx.permissions.check(op, req.user)) {
+              return next('Not permitted');
+            }
+            where = {};
+            if (req.params.id) {
+              where[ndx.settings.AUTO_ID] = req.params.id;
+            }
+            ndx.database.upsert(tableName, req.body, where);
+            return res.end('OK');
+          };
         };
-        deleteFn = function(req, res, next) {
-          if (ndx.permissions && !ndx.permissions.check('delete', req.user)) {
-            return next('Not permitted');
-          }
-          if (req.params.id) {
-            return ndx.database["delete"](tableName, req.params.id);
-          }
+        deleteFn = function(tableName) {
+          return function(req, res, next) {
+            if (ndx.permissions && !ndx.permissions.check('delete', req.user)) {
+              return next('Not permitted');
+            }
+            if (req.params.id) {
+              ndx.database["delete"](tableName, req.params.id);
+            }
+            return res.end('OK');
+          };
         };
-        ndx.app.get(["/api/" + tableName, "/api/" + tableName + "/:id"], ndx.authenticate(auth), selectFn);
-        ndx.app.post("/api/" + tableName + "/search", ndx.authenticate(auth), selectFn);
-        ndx.app.post(["/api/" + tableName, "/api/" + tableName + "/:id"], ndx.authenticate(auth), upsertFn);
-        ndx.app.put(["/api/" + tableName, "/api/" + tableName + "/:id"], ndx.authenticate(auth), upsertFn);
-        results.push(ndx.app["delete"]("/api/" + tableName + "/:id", ndx.authenticate(auth), deleteFn));
+        ndx.app.get(["/api/" + tableName, "/api/" + tableName + "/:id"], ndx.authenticate(auth), selectFn(tableName));
+        ndx.app.post("/api/" + tableName + "/search", ndx.authenticate(auth), selectFn(tableName));
+        ndx.app.post(["/api/" + tableName, "/api/" + tableName + "/:id"], ndx.authenticate(auth), upsertFn(tableName));
+        ndx.app.put(["/api/" + tableName, "/api/" + tableName + "/:id"], ndx.authenticate(auth), upsertFn(tableName));
+        results.push(ndx.app["delete"]("/api/" + tableName + "/:id", ndx.authenticate(auth), deleteFn(tableName)));
       }
       return results;
     });
--- src/index.coffee
+++ src/index.coffee
@@ -3,6 +3,14 @@
 module.exports = (ndx) ->
   ndx.rest = {}
   setImmediate ->
+    ndx.app.get '/rest/endpoints', (req, res, next) ->
+      endpoints = ndx.rest.tables or ndx.settings.REST_TABLES or ndx.settings.TABLES
+      if endpoints and endpoints.length and Object.prototype.toString.call(endpoints[0]) is '[object Array]'
+        for endpoint in endpoints
+          endpoint = endpoint[0]
+      res.json 
+        autoId: ndx.settings.AUTO_ID
+        endpoints: endpoints
     for table in ndx.rest.tables or ndx.settings.REST_TABLES or ndx.settings.TABLES
       type = Object.prototype.toString.call table
       tableName = ''
@@ -12,41 +20,46 @@ module.exports = (ndx) ->
       else if type is '[object Array]'
         tableName = table[0]
         auth = table[1]
-      selectFn = (req, res, next) ->
-        if ndx.permissions and not ndx.permissions.check('select', req.user)
-          return next('Not permitted')
-        if req.params and req.params.id
-          where = {}
-          where[ndx.settings.AUTO_ID] = req.params.id
-          items = ndx.database.select tableName, where
-          if items and items.length
-            res.json items[0]
+      selectFn = (tableName) ->
+        (req, res, next) ->
+          if ndx.permissions and not ndx.permissions.check('select', req.user)
+            return next('Not permitted')
+          if req.params and req.params.id
+            where = {}
+            where[ndx.settings.AUTO_ID] = req.params.id
+            items = ndx.database.select tableName, where
+            if items and items.length
+              res.json items[0]
+            else
+              res.end 'Nothing found'
           else
-            res.end 'Nothing found'
-        else
-          res.json
-            total: ndx.database.count tableName, req.body.where
-            page: req.body.page or 1
-            pageSize: req.body.pageSize or 0
-            items: ndx.database.select tableName, req.body.where, req.body.page, req.body.pageSize, req.body.sort, req.body.sortDir
-      upsertFn = (req, res, next) ->
-        op = if req.params.id then 'update' else 'insert'
-        if ndx.permissions and not ndx.permissions.check(op, req.user)
-          return next('Not permitted')
-        if req.params.id
-          req.body[ndx.settings.AUTO_ID] = req.params.id
-        ndx.database.upsert tableName, req.body
-        res.end 'OK'
-      deleteFn = (req, res, next) ->
-        if ndx.permissions and not ndx.permissions.check('delete', req.user)
-          return next('Not permitted')
-        if req.params.id
-          ndx.database.delete tableName, req.params.id
-      ndx.app.get ["/api/#{tableName}", "/api/#{tableName}/:id"], ndx.authenticate(auth), selectFn
-      ndx.app.post "/api/#{tableName}/search", ndx.authenticate(auth), selectFn
-      ndx.app.post ["/api/#{tableName}", "/api/#{tableName}/:id"], ndx.authenticate(auth), upsertFn
-      ndx.app.put ["/api/#{tableName}", "/api/#{tableName}/:id"], ndx.authenticate(auth), upsertFn
-      ndx.app.delete "/api/#{tableName}/:id", ndx.authenticate(auth), deleteFn
+            res.json
+              total: ndx.database.count tableName, req.body.where
+              page: req.body.page or 1
+              pageSize: req.body.pageSize or 0
+              items: ndx.database.select tableName, req.body.where, req.body.page, req.body.pageSize, req.body.sort, req.body.sortDir
+      upsertFn = (tableName) ->
+        (req, res, next) ->
+          op = if req.params.id then 'update' else 'insert'
+          if ndx.permissions and not ndx.permissions.check(op, req.user)
+            return next('Not permitted')
+          where = {}
+          if req.params.id
+            where[ndx.settings.AUTO_ID] = req.params.id
+          ndx.database.upsert tableName, req.body, where
+          res.end 'OK'
+      deleteFn = (tableName) ->
+        (req, res, next) ->
+          if ndx.permissions and not ndx.permissions.check('delete', req.user)
+            return next('Not permitted')
+          if req.params.id
+            ndx.database.delete tableName, req.params.id
+          res.end 'OK'
+      ndx.app.get ["/api/#{tableName}", "/api/#{tableName}/:id"], ndx.authenticate(auth), selectFn(tableName)
+      ndx.app.post "/api/#{tableName}/search", ndx.authenticate(auth), selectFn(tableName)
+      ndx.app.post ["/api/#{tableName}", "/api/#{tableName}/:id"], ndx.authenticate(auth), upsertFn(tableName)
+      ndx.app.put ["/api/#{tableName}", "/api/#{tableName}/:id"], ndx.authenticate(auth), upsertFn(tableName)
+      ndx.app.delete "/api/#{tableName}/:id", ndx.authenticate(auth), deleteFn(tableName)
         
       
         
